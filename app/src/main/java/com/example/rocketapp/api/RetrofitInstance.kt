f42b2f10db7cfc5e85c29a39dc189853b8812327
Martin Beran <martinrohatej@gmail.com>
2022-02-23 14:43:16 +0100

    FEAT: format by ktlint

--- app/src/main/java/com/example/rocketapp/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/rocketapp/api/RetrofitInstance.kt
@@ -7,7 +7,8 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Date
+import java.util.Locale
 import java.util.concurrent.TimeUnit
 
 class DateAdapter {
--- app/src/main/java/com/example/rocketapp/api/dto/RocketItemDto.kt
+++ app/src/main/java/com/example/rocketapp/api/dto/RocketItemDto.kt
@@ -2,7 +2,7 @@ package com.example.rocketapp.api.dto
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import java.util.*
+import java.util.Date
 
 @JsonClass(generateAdapter = true)
 data class RocketItemDto(
